a4a0d257066e44bcaba0e90f5e548db95ad49ec5
Juan Lemus <jlemus002@student.butte.edu>
2016-08-28 06:35:14 +0000

    Lab2 submission

---
+++ Lab2/lab2.cpp
@@ -0,0 +1,14 @@
+//Juan Lemus
+//8/25/16
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    /* "Computer chooses a number between 10 and 50"
+       "User guesses a number"
+       "If user is correct they win"
+       "If user is incorrect the computer tells them if the number is higher or lower than their selection"
+       "Repeat until user is correct" */
+}
