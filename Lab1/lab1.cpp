0a75fa2d5dacb0479db5493f47c4a75dc20818c1
Juan Lemus <jlemus002@student.butte.edu>
2016-08-28 06:23:26 +0000

    Lab 1 ASCII art

---
+++ Lab1/lab1.cpp
@@ -0,0 +1,30 @@
+//Juan Lemus
+//8/24/16
+//CSCI20
+//Browne
+#include <iostream>
+using namespace std;
+
+int main ()
+
+{
+    cout <<"Common graffiti in middle school desks:"<< endl;
+    cout <<"      xx     "<< endl;
+    cout <<"    xx  xx   "<< endl;
+    cout <<"   xx    xx  "<< endl;
+    cout <<"  xx      xx "<< endl;
+    cout <<" xx   xx   xx"<< endl;
+    cout <<" xx   xx   xx"<< endl;
+    cout <<" xx   xx   xx"<< endl;
+    cout <<" xxx   xx  xx"<< endl;
+    cout <<" xxxx   xx xx"<< endl;
+    cout <<" xx  xx   xxx"<< endl;
+    cout <<" xx   xx   xx"<< endl;
+    cout <<" xx   xx   xx"<< endl;
+    cout <<" xx   xx   xx"<< endl;
+    cout <<" xx        xx"<< endl;
+    cout <<"  xx      xx "<< endl;
+    cout <<"   xx    xx  "<< endl;
+    cout <<"    xx  xx   "<< endl;
+    cout <<"      xx     "<< endl;
+    }
