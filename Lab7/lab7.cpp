a9a103c4694c306dd3de3461690a519724c6848a
Juan Lemus <jlemus002@student.butte.edu>
2016-09-21 17:59:57 +0000

    lab7 submission

--- Lab7/lab7.cpp
+++ Lab7/lab7.cpp
@@ -1,37 +1,36 @@
 // Creator: Juan Lemus  
 // Browne
 // Date created: 9/19/16
-
+// Wheels on the Bus Nursery Ryhme
 #include <iostream>
+#include <string>
 using namespace std;
 
-void VerseA(int carPart, int partAction)
+void verseA(string carPart, string partAction)
 {
-  cin >> carPart;
-  cin >> partAction; 
+    
   cout <<"The " << carPart <<" on the bus go " << partAction << " and " << partAction << endl;
   cout << partAction <<" and " << partAction << endl;
   cout << partAction <<" and " << partAction << endl;
   cout <<"The " << carPart <<" on the bus go " << partAction << " and " << partAction << endl;
-  cout << "All round the town"
+  cout << "All round the town" <<endl;
 
 }
 
-void ChorusA (int carPart2, int partAction2)
+void chorusA (string carPart2, string partAction2)
 {
-    cin >> carPart2;
-    cin >> partAction2;
+
     cout << "The " << carPart2 << " on the bus go " <<partAction2 <<", " << partAction2 << ", " << partAction2 << endl;
     cout << partAction2 <<", " << partAction2 <<", " << partAction2 << endl;
     cout << partAction2 <<", " << partAction2 <<", " << partAction2 << endl;
     cout << "The " << carPart2 << " on the bus go " <<partAction2 <<", " << partAction2 << ", " << partAction2 << endl;
-    cout << "All round the town";
+    cout << "All round the town" << endl;
 }
 
 int main()
 {
-    VerseA();
-    ChorusA();
+    verseA("wheels", "round");
+    chorusA("wipers", "swish");
     
     return 0;
     
