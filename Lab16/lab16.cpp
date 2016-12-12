040a249c75f758d85a89404657741aebf6bb5b8b
Juan Lemus <jlemus002@student.butte.edu>
2016-12-12 06:03:15 +0000

    Lab 14 submission

---
+++ Lab16/lab16.cpp
@@ -0,0 +1,35 @@
+//Juan Lemus
+//Parallel Arrays: create a product price lookup.
+//Date: 11/1/16
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ /*   int prodPrice [10];
+    int nthProduct = 0;
+    
+    prodPrice [0] = 15;
+    prodPrice [1] = 50;
+    prodPrice [2] = 25;
+    prodPrice [3] = 60;
+    prodPrice [4] = 100;
+    prodPrice [5] = 45;
+    prodPrice [6] = 85;
+    prodPrice [7] = 58;
+    prodPrice [8] = 79;
+    prodPrice [9] = 31;
+    
+    cout << "Choose product (0-9)";
+    cin >> nthProduct;
+    
+    if ((nthProduct >= 1) && (nthProduct<=5)) {
+        cout << "The " << nthProduct << "th product's price is ";
+        cout << prodPrice[nthProduct - 1] << " Dollars." << endl;
+    }*/
+string myString = "my test string";
+myString[14] = '!';
+cout<<myString<<endl;
+    return 0;
+}
